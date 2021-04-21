88d551366bfe3c285d8a565709e15709488b1fd0
xl_du@shanchain.com <duxl20210104>
2021-04-21 16:44:59 +0800

    请求头添加参数

--- src/main/java/com/duxl/baselib/utils/NetCheckUtil.java
+++ src/main/java/com/duxl/baselib/utils/NetCheckUtil.java
@@ -5,9 +5,17 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.telephony.TelephonyManager;
 
 import androidx.core.app.ActivityCompat;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 /**
  * 判断wifi 还是 移动网络~
@@ -200,5 +208,55 @@ public class NetCheckUtil {
         }
     }
 
+    /**
+     * 获取IP地址
+     * @return
+     */
+    public static String getIPAddress() {
+        try {
+            NetworkInfo info = ((ConnectivityManager) Utils.getContext()
+                    .getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
+            if (info != null && info.isConnected()) {
+                if (info.getType() == ConnectivityManager.TYPE_MOBILE) {    // 当前使用2G/3G/4G网络
+                    try {
+                        for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements(); ) {
+                            NetworkInterface intf = en.nextElement();
+                            for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements(); ) {
+                                InetAddress inetAddress = enumIpAddr.nextElement();
+                                if (!inetAddress.isLoopbackAddress() && inetAddress instanceof Inet4Address) {
+                                    return inetAddress.getHostAddress();
+                                }
+                            }
+                        }
+                    } catch (SocketException e) {
+                        e.printStackTrace();
+                    }
 
+                } else if (info.getType() == ConnectivityManager.TYPE_WIFI) {    // 当前使用无线网络
+                    WifiManager wifiManager = (WifiManager) Utils.getContext().getSystemService(Context.WIFI_SERVICE);
+                    WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+                    String ipAddress = intIP2StringIP(wifiInfo.getIpAddress());    // 得到IPV4地址
+                    return ipAddress;
+                }
+            } else {
+                // 当前无网络连接,请在设置中打开网络
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将得到的int类型的IP转换为String类型
+     *
+     * @param ip
+     * @return
+     */
+    public static String intIP2StringIP(int ip) {
+        return (ip & 0xFF) + "." +
+                ((ip >> 8) & 0xFF) + "." +
+                ((ip >> 16) & 0xFF) + "." +
+                (ip >> 24 & 0xFF);
+    }
 }
