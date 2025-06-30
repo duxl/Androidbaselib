6346fa43436b6db9515f5f19a40082cc0f20ce58
duxl <645575165@qq.com>
2025-06-30 11:03:43 +0800

    SmartRecyclerView支持在xml中指定LayoutManager

--- src/main/java/com/duxl/baselib/widget/SmartRecyclerView.java
+++ src/main/java/com/duxl/baselib/widget/SmartRecyclerView.java
@@ -20,6 +20,9 @@ import com.duxl.baselib.ui.status.SimpleStatusView;
 import com.scwang.smart.refresh.footer.ClassicsFooter;
 import com.scwang.smart.refresh.header.ClassicsHeader;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * SmartRecycler 封装了上拉刷新，下载加载更多，显示各种状态的列表
  * create by duxl 2020/8/16
@@ -28,6 +31,8 @@ public class SmartRecyclerView extends XSmartRefreshLayout implements IRefreshCo
 
     protected RecyclerView mRecyclerView;
     protected IStatusView mStatusView;
+    private static final Class<?>[] LAYOUT_MANAGER_CONSTRUCTOR_SIGNATURE =
+            new Class<?>[]{Context.class, AttributeSet.class, int.class, int.class};
 
     public SmartRecyclerView(Context context) {
         this(context, null);
@@ -45,6 +50,12 @@ public class SmartRecyclerView extends XSmartRefreshLayout implements IRefreshCo
             }
         }
 
+        String layoutManagerName = typedArray.getString(R.styleable.SmartRecyclerView_layoutManager);
+        if (layoutManagerName == null) {
+            mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        } else {
+            createLayoutManager(context, layoutManagerName, attrs, 0, 0);
+        }
 
         typedArray.recycle();
     }
@@ -57,13 +68,84 @@ public class SmartRecyclerView extends XSmartRefreshLayout implements IRefreshCo
         frameLayout.setId(R.id.status_view_container);
 
         mRecyclerView = new RecyclerView(context);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
         frameLayout.addView(mRecyclerView, FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
 
         mStatusView = initStatusView();
         frameLayout.addView(mStatusView.getView(), FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
 
         addView(frameLayout, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+    }
+
+    /**
+     * 从RecyclerView拷贝过来的代码
+     *
+     * @param context
+     * @param className
+     * @param attrs
+     * @param defStyleAttr
+     * @param defStyleRes
+     */
+    private void createLayoutManager(Context context, String className, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        if (className != null) {
+            className = className.trim();
+            if (!className.isEmpty()) {
+                className = getFullClassName(context, className);
+                try {
+                    ClassLoader classLoader;
+                    if (isInEditMode()) {
+                        // Stupid layoutlib cannot handle simple class loaders.
+                        classLoader = this.getClass().getClassLoader();
+                    } else {
+                        classLoader = context.getClassLoader();
+                    }
+                    Class<? extends RecyclerView.LayoutManager> layoutManagerClass =
+                            Class.forName(className, false, classLoader)
+                                    .asSubclass(RecyclerView.LayoutManager.class);
+                    Constructor<? extends RecyclerView.LayoutManager> constructor;
+                    Object[] constructorArgs = null;
+                    try {
+                        constructor = layoutManagerClass
+                                .getConstructor(LAYOUT_MANAGER_CONSTRUCTOR_SIGNATURE);
+                        constructorArgs = new Object[]{context, attrs, defStyleAttr, defStyleRes};
+                    } catch (NoSuchMethodException e) {
+                        try {
+                            constructor = layoutManagerClass.getConstructor();
+                        } catch (NoSuchMethodException e1) {
+                            e1.initCause(e);
+                            throw new IllegalStateException(attrs.getPositionDescription()
+                                    + ": Error creating LayoutManager " + className, e1);
+                        }
+                    }
+                    constructor.setAccessible(true);
+                    mRecyclerView.setLayoutManager(constructor.newInstance(constructorArgs));
+                } catch (ClassNotFoundException e) {
+                    throw new IllegalStateException(attrs.getPositionDescription()
+                            + ": Unable to find LayoutManager " + className, e);
+                } catch (InvocationTargetException e) {
+                    throw new IllegalStateException(attrs.getPositionDescription()
+                            + ": Could not instantiate the LayoutManager: " + className, e);
+                } catch (InstantiationException e) {
+                    throw new IllegalStateException(attrs.getPositionDescription()
+                            + ": Could not instantiate the LayoutManager: " + className, e);
+                } catch (IllegalAccessException e) {
+                    throw new IllegalStateException(attrs.getPositionDescription()
+                            + ": Cannot access non-public constructor " + className, e);
+                } catch (ClassCastException e) {
+                    throw new IllegalStateException(attrs.getPositionDescription()
+                            + ": Class is not a LayoutManager " + className, e);
+                }
+            }
+        }
+    }
+
+    private String getFullClassName(Context context, String className) {
+        if (className.charAt(0) == '.') {
+            return context.getPackageName() + className;
+        }
+        if (className.contains(".")) {
+            return className;
+        }
+        return RecyclerView.class.getPackage().getName() + '.' + className;
     }
 
     protected IStatusView initStatusView() {
